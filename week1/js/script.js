c3c8f4d8945da3b02d2c5cad78d102aeac38436a
LuigiMagdamitUCLA <luigiantonmagdamit@gmail.com>
2021-06-25 13:17:43 -0700

    lab 1 draft

---
+++ week1/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript const variable declaration
+const map = L.map('map').setView([34.0709, -118.444], 15);
+
+// Leaflet tile layer, i.e. the base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//JavaScript let variable declaration to create a marker
+let marker = L.marker([33.875671, -117.568840]).addTo(map)
+		.bindPopup('lololol')
+		.openPopup();
